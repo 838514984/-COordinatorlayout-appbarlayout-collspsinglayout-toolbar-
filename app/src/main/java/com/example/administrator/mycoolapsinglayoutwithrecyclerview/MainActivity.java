57492d1486dcf4baa5230b38c3c183347cb1c430
secretbase <838514984@qq.com>
2017-07-19 16:56:36 +0800

    加入smartRefreshlayout

--- app/src/main/java/com/example/administrator/mycoolapsinglayoutwithrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/administrator/mycoolapsinglayoutwithrecyclerview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.mycoolapsinglayoutwithrecyclerview;
 
+import android.os.Handler;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
@@ -14,6 +15,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,58 +32,78 @@ public class MainActivity extends AppCompatActivity {
     CoordinatorLayout coordinatorLayout;
     TextView hint;
     Menu menu;
+    SmartRefreshLayout smartRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.rc);
-        appBarLayout = (AppBarLayout) findViewById(R.id.appbarlayout);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        hint = (TextView) findViewById(R.id.tv_hint);
-        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
+        findviews();
         setSupportActionBar(toolbar);
         StatusBarUtil.immersive(this);
         StatusBarUtil.setPaddingSmart(this, toolbar);
         inidatas();
-        myAdapter = new MyAdapter(mdatas, this, new MyAdapter.MyListener() {
+        initAdapter();
+        initAppbarLayoutListener();//judge sliding status;
+        initSmartRefreshListener();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+    }
+
+    private void initSmartRefreshListener() {
+        smartRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
-            public void onClick(View view) {
-                flag = !flag;
-                if (flag) {
-                    appBarLayout.setExpanded(true, true);
-                } else {
-                    appBarLayout.setExpanded(false, true);
-                }
+            public void onRefresh(final RefreshLayout refreshlayout) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mdatas.add(0,"this is the new data inserted");
+                        myAdapter.notifyDataSetChanged();
+                        refreshlayout.finishRefresh();
+                    }
+                },1000);
             }
         });
+    }
 
+    private void initAppbarLayoutListener() {
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                //展开
-                if (Math.abs(verticalOffset) >= 0 && Math.abs(verticalOffset) < appBarLayout.getTotalScrollRange()) {
+                //展开中
+                if (Math.abs(verticalOffset) > 0 && Math.abs(verticalOffset) < appBarLayout.getTotalScrollRange()) {
                     toolbar.setTitle("this is the title");
                     toolbar.setNavigationIcon(R.mipmap.ic_drawer_home);
                     hint.setVisibility(View.GONE);
                     if (menu != null)
-                        showMenu();;
+                        showMenu();
                     ;
+                    smartRefreshLayout.setEnableRefresh(false);
+                } else if (verticalOffset == 0) {
+                    //展开到头
+                    smartRefreshLayout.setEnableRefresh(true);
                 } else {
+                    //折疊
                     toolbar.setTitle("");
                     toolbar.setNavigationIcon(null);
                     hideMenu();
                     hint.setVisibility(View.VISIBLE);
+                    smartRefreshLayout.setEnableRefresh(false);
                 }
                 Log.e("xxx", "verticaloffset: " + verticalOffset);
                 Log.e("xxx", "appBarLayout.getTotalScrollRange(): " + appBarLayout.getTotalScrollRange() + "\n");
             }
         });
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(myAdapter);
     }
 
+    private void findviews(){
+        recyclerView = (RecyclerView) findViewById(R.id.rc);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarlayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        hint = (TextView) findViewById(R.id.tv_hint);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
+        smartRefreshLayout = (SmartRefreshLayout) findViewById(R.id.srl);
+    }
     private void showMenu() {
         for (int i = 0; i < menu.size(); i++) {
             menu.getItem(i).setVisible(true);
@@ -96,7 +121,19 @@ public class MainActivity extends AppCompatActivity {
             mdatas.add("this is the " + i + " position item");
         }
     }
-
+    private void initAdapter(){
+        myAdapter = new MyAdapter(mdatas, this, new MyAdapter.MyListener() {
+            @Override
+            public void onClick(View view) {
+                flag = !flag;
+                if (flag) {
+                    appBarLayout.setExpanded(true, true);
+                } else {
+                    appBarLayout.setExpanded(false, true);
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
